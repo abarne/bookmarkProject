8792bf04fcc70ebc299038de6efcf8f801c4c3c3
abarne <celius377@gmail.com>
2020-01-13 15:34:01 -0600

    cors issues

--- api/server.js
+++ api/server.js
@@ -9,15 +9,30 @@ const linkRouter = require('../data/links/link-router.js');
 
 const server = express();
 
-server.use(cors());
-
 server.use(helmet());
+server.use(cors());
 server.use(express.json());
+
+// const corsOptions = {
+// 	origin: '*'
+// };
 
 server.use('/api/auth', authRouter);
 server.use('/api/main', mainRouter);
 server.use('/api/sub', subRouter);
 server.use('/api/link', linkRouter);
+
+// server.use(cors(corsOptions));
+// server.use(function(req, res, next) {
+// 	res.header('Access-Control-Allow-Origin', '*');
+// 	res.header('Access-Control-Allow-Credentials', true);
+// 	res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE, OPTIONS');
+// 	res.header(
+// 		'Access-Control-Allow-Headers',
+// 		'Origin, X-Requested-With, Content-Type, content-type, application/json'
+// 	);
+// 	next();
+// });
 
 server.get('/', (req, res) => {
 	res.status(200).json({ message: 'server is working' });
