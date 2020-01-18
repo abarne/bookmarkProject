e9ea322d5aa8277f5e0174ce982986edad52c81e
abarne <celius377@gmail.com>
2020-01-17 20:45:07 -0600

    1

--- knexfile.js
+++ knexfile.js
@@ -11,13 +11,10 @@ module.exports = {
 	},
 	production: {
 		client: 'pg',
-		connection: {
-			database: 'de3i0svfirhp2p',
-			user: 'rltzgeqvynynbu',
-			password: '5b6d253a56e65183a8ecb6fbd489543e2ea117e6d9448ecc227bccffae8fb31e'
-		},
+		connection: 'process.env.DATABASE_URL',
 		migrations: {
 			directory: './database/migrations'
-		}
+		},
+		useNullAsDefault: true
 	}
 };
